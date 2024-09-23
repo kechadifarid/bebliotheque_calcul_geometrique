be10177c3b8ba624de2ea7b7d9b3f297926826ed
uapv2307875@ad.univ-avignon.fr <benjaminb4@outlook.fr>
2024-09-23 17:28:00 +0200

    merge des setLeght et setWidth en setData + ajout segment

--- Rectangle.java
+++ Rectangle.java
@@ -1,7 +1,5 @@
 package Calculer_Distance;
 
-import java.io.IOException;
-
 public class Rectangle {
     private double lenght;
     private double width;
@@ -19,7 +17,7 @@ public class Rectangle {
         this.bottom_right = new Point();
         this.upper_left = new Point();
         this.bottom_left = new Point();
-    };
+    }
 
     public double getLenght() {
         return lenght;
@@ -37,21 +35,20 @@ public class Rectangle {
         return y;
     }
 
-    public void setLenght(double lenght) throws NoCoordException {
+    public void setData(double lenght, double width) throws NoCoordException {
         this.lenght = lenght;
-        if(getX()==0 || getY()==0)  throw new NoCoordException("Renseignez les coordonnées");
-
-        upper_right.setX(getX()+lenght);
-        upper_right.setY(getY());
-    }
-
-    public void setWidth(double width) throws NoCoordException {
         this.width = width;
         if(getX()==0 || getY()==0)  throw new NoCoordException("Renseignez les coordonnées");
-            bottom_left.setY(getY() - width);
-            bottom_left.setX(getX());
-            bottom_right.setX(upper_right.getX());
-            bottom_right.setY(bottom_left.getY());
+        upper_right.setX(getX()+lenght);
+        upper_right.setY(getY());
+        bottom_left.setY(getY() - width);
+        bottom_left.setX(getX());
+        bottom_right.setX(upper_right.getX());
+        bottom_right.setY(bottom_left.getY());
+        Segment s1 = new Segment(upper_left,upper_right);
+        Segment s2 = new Segment(upper_left,bottom_left);
+        Segment s3 = new Segment(bottom_left,bottom_right);
+        Segment s4 = new Segment(upper_right,bottom_right);
 
 
 
@@ -65,5 +62,8 @@ public class Rectangle {
     public void setY(double y) {
         this.y = y;
         upper_left.setY(y);
+    }
+     public static void main(String[] args) {
+
     }
 }
