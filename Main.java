74cca915ac71368b4c90607446c00b8424dba090
kecha <kechadifarid10@gmail.com>
2024-09-23 17:08:48 +0200

    Definir Carre , calculer les diff point

---
+++ Main.java
@@ -0,0 +1,10 @@
+package Calculer_Distance;
+
+public class Main {
+    public static void main(String[] args) {
+        Point A = new Point(1,2);
+        Carre c = new Carre(A,3);
+        c.getPoints();
+
+    }
+}
