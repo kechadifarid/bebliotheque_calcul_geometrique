a441ab7ed6fa5cc8e53423c26ce0a822c86837e4
akrambourouina <akram.bourouina@alumni.univ-avignon.fr>
2024-11-19 16:10:31 +0100

    pom.xml test

---
+++ src/main/test/java/bibliotheque/PolygonTest.java
@@ -0,0 +1,15 @@
+package bibliotheque;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PolygonTest {
+
+    @Test
+    void essai()
+    {
+        assertEquals(0,0);
+    }
+
+}
